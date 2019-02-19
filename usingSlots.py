3f45308cac3e5344132e163189a5c6ff36ad7b63
Alex Vaskov <avask@iMac-Alex.local>
2019-02-19 20:11:26 +0300

    More Tips for __slots__

--- usingSlots.py
+++ usingSlots.py
@@ -15,3 +15,12 @@ class almostPOD():
     
     def __repr__(self):
         return str(self)
+    
+    ## <!> ACHTUNG: When using __slots__, instances of the class
+    #  will not be allowed to have any other attributes but those
+    #  specified in __slots__. That's kinda a drawback, but it's 
+    #  solveable. [see NOTE]
+    
+    ## <!> NOTE: you can add '__dict__' to __slots__ iterable,
+    #  so it will support both methods, but you risk cancelling the 
+    #  traits of __slots__ then. Always stay vigilant.
